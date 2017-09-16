87506bde4a563562f49b26a75c8e2c5a4b291f58
TiCubius <ticubius@gmail.com>
2017-09-17 00:20:04 +0200

    2.15b - 17/09/2017 - INDEV

--- app/database.js
+++ app/database.js
@@ -1,6 +1,6 @@
-// TrashBOT - app/database.js
-// VERSION: 2.10 [25/08/2017]
-// AUTHOR: TiCubius <trashmates@protonmail.com>
+// TrashBOT / app/database.js
+// VERSION: 2.15
+// TiCubius <trashmates@protonmail.com>
 
 const mysql = require("mysql")
 const settings = require("../config/settings")
